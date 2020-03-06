dd5ca33510e93e9477cdf9f1f9228e0d30d82a48
Martin Ošťádal <ostadam@saftbatteries.com>
2020-03-06 21:11:36 +0100

    hello world

--- Jebnuta kalkulacka/Program.cs
+++ Jebnuta kalkulacka/Program.cs
@@ -5,52 +5,67 @@ namespace Kalkulacka
     class Program
     {
         static void Main(string[] args)
-        {
-            /*Console.WriteLine("Hello World!");
-            string s ="50";
-            int a = int.Parse(s);
-            double x = Math.Sqrt(a);
-            Console.WriteLine(x); */
-            
-            Console.WriteLine("Obycejna kalkulacka, kde pouzivame switch");
-            Console.WriteLine("Prvni cislo: ");
-            float a = float.Parse(Console.ReadLine());
-            Console.WriteLine("Druhe cislo: ");
-            float b = float.Parse(Console.ReadLine());
-            if (a == 0 || b == 0)
-            {
-                Console.WriteLine("Nemuzes delit nulou, proto nevol tuto moznost!");
-            }
-            Console.WriteLine("Zvol moznost operace, kterou chces provest: ");
-            Console.WriteLine("1 - Secti");
-            Console.WriteLine("2 - Odecti");
-            Console.WriteLine("3 - Vynasob");
-            Console.WriteLine("4 - Vydel");
-            int moznost = int.Parse(Console.ReadLine());;
+        {   bool sprMoznost = true, pokracuj = true;
+            int moznost = 0;
             float vysledek = 0;
-            switch (moznost)
+
+            while (pokracuj)
             {
-                case 1:
-                    vysledek = a + b;
-                break;
+                    
+                Console.WriteLine("Obycejna kalkulacka, kde pouzivame switch");
+                Console.WriteLine("Prvni cislo: ");
+                float a = float.Parse(Console.ReadLine());
+                Console.WriteLine("Druhe cislo: ");
+                float b = float.Parse(Console.ReadLine());
+                if (b == 0)
+                {
+                    Console.WriteLine("Nemuzes delit nulou, proto nevol tuto moznost!");
+                }
+                while (sprMoznost)
+                {
+                    sprMoznost = false;              
+                
+                    Console.WriteLine("Zvol moznost operace, kterou chces provest: ");
+                    Console.WriteLine("1 - Secti");
+                    Console.WriteLine("2 - Odecti");
+                    Console.WriteLine("3 - Vynasob");
+                    Console.WriteLine("4 - Vydel");
+                    Console.WriteLine("5 - Ukoncit?");
+                    moznost = int.Parse(Console.ReadLine());
+                    vysledek = 0;
+                    switch (moznost)
+                    {
+                        case 1:
+                            vysledek = a + b;
+                        break;
 
-                case 2:
-                    vysledek = a - b;
-                break;
+                        case 2:
+                            vysledek = a - b;
+                        break;
 
-                case 3:
-                    vysledek = a * b;
-                break;
+                        case 3:
+                            vysledek = a * b;
+                        break;
 
-                case 4:
-                    vysledek = a / b;
-                break;
-            }
-             if ((a == 0 || b == 0) && (moznost == 4))
-                 Console.WriteLine("Tak hele debilku, nulou se proste delit neda, chapes to???");
-             else
-                 Console.WriteLine("Vysledek je: " + vysledek);
-                 Console.ReadKey();
+                        case 4:
+                            vysledek = a / b;
+                        break;
+
+                        case 5:
+                            Environment.Exit(0);
+                        break;
+
+                        default:
+                            sprMoznost = true;
+                        break;         
+                    }
+                }
+                if ((b == 0) && (moznost == 4))
+                    Console.WriteLine("Tak hele debilku, nulou se proste delit neda, chapes to???");
+                else
+                    Console.WriteLine("Vysledek je: " + vysledek);
+                    Console.ReadKey();
+            }         
              
         }   
 
